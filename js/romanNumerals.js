2356fed4a9f302087191737f7cfe94541701bcc8
kineticvenom <kalebvarnes@yahoo.com>
2022-05-25 09:16:42 -1000

    Added Code

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,31 @@
 exports.toRoman = function(num) {
-
+    exports.factorial = function(num) {
+        let arab = num
+        let answer = []
+        let romansObj = {
+            M: '1000',
+            CM: '900',
+            D: '500',
+            CD:'400',
+            C: '100',
+            XC: '90',
+             L: '50',
+            XL: '40',
+            X: '10',
+            IX: '9',
+            V: '5',
+            IV: '4',
+            I: '1',
+        }
+    
+      for (let roman of Object.keys(romansObj)) {
+        let arab = Math.floor(num / romansObj[roman]);
+        num -= arab * romansObj[roman];
+        answer.push(roman.repeat(arab))
+      }
+    
+    return answer.join('').toString();
+            }
+    
+              
 };
